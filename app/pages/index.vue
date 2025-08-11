194be09dcb206a4c269b2b17efcfc09de7e686fe
John Martin <74799962+jhnmartin@users.noreply.github.com>
2025-08-11 12:07:38 -0500

    update images

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,6 +19,48 @@ const links = [
       orientation="horizontal"
       class="font-display"
       :links="links"
-    />
+    >
+      <img
+        src="/images/hero.webp"
+        alt="Hero Image"
+        class="rounded-lg shadow-2xl ring ring-default"
+      />
+    </UPageHero>
+    <UPageSection
+      title="Upcoming Events"
+      description="Find Upcoming Events Near You"
+    >
+      <UPageMarquee class="py-4">
+        <UCard>
+          <template #header>
+            <img
+              src="/images/hero.webp"
+              alt="Hero Image"
+              class="h-96 rounded-lg shadow-2xl ring ring-default p-0"
+            />
+          </template>
+          <h3>Test</h3>
+          <template #footer>
+            <p>footer</p>
+          </template>
+        </UCard>
+
+        <img
+          src="/images/hero.webp"
+          alt="Hero Image"
+          class="h-96 rounded-lg shadow-2xl ring ring-default"
+        />
+        <img
+          src="/images/hero.webp"
+          alt="Hero Image"
+          class="h-96 rounded-lg shadow-2xl ring ring-default"
+        />
+        <img
+          src="/images/hero.webp"
+          alt="Hero Image"
+          class="h-96 rounded-lg shadow-2xl ring ring-default"
+        />
+      </UPageMarquee>
+    </UPageSection>
   </div>
 </template>
